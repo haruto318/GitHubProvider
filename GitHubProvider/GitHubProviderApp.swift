0272ff686a41fd3758b05d0a69551961e02be5e5
Haruto Hamano <haruto.yhs318@gmail.com>
2024-04-03 00:06:17 +0900

    Initial Commit

---
+++ GitHubProvider/GitHubProviderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubProviderApp.swift
+//  GitHubProvider
+//
+//  Created by 濱野遥斗 on 2024/04/03.
+//
+
+import SwiftUI
+
+@main
+struct GitHubProviderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
